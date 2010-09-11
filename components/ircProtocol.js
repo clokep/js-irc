99d02246ca2734294da2c61f41fe7ca51381313a
Patrick Cloke <clokep@gmail.com>
2010-09-11 16:47:27 -0400

    Fix spacing Sending messages now works

--- components/ircProtocol.js
+++ components/ircProtocol.js
@@ -56,102 +56,100 @@ var Cc = Components.classes;
 var Ci = Components.interfaces;
 
 function dump(str) {
-	Cc["@mozilla.org/consoleservice;1"]
-		.getService(Ci.nsIConsoleService)
-		.logStringMessage(str);
+  Cc["@mozilla.org/consoleservice;1"]
+    .getService(Ci.nsIConsoleService)
+    .logStringMessage(str);
 }
 
 function Conversation(aAccount) {
-	this._init(aAccount);
+  this._init(aAccount);
 }
 Conversation.prototype = {
-	sendMsg: function(aMsg) {
-		dump("Send");
-		dump(aMsg);
-		dump(this.account.outputStream.write(aMsg, aMsg.length));
-		this.writeMessage("Me",aMsg,{outgoing: true});
-		dump("Send end");
-	},
-	get name() "IRC"
+  sendMsg: function(aMsg) {
+    aMsg += "\r\n";
+    dump("Send");
+    dump(aMsg);
+    this.account.outputStream.write(aMsg, aMsg.length);
+    this.writeMessage("Me",aMsg,{outgoing: true});
+    dump("Send end");
+  },
+  get name() "IRC"
 };
 Conversation.prototype.__proto__ = GenericConversationPrototype;
 
 function Account(aProtoInstance, aKey, aName) {
-	this._init(aProtoInstance, aKey, aName);
+  this._init(aProtoInstance, aKey, aName);
 }
 Account.prototype = {
-	_conv: null,
-	inputStream: null,
-	outputStream: null,
-	scritableInputStream: null,
-	pump: null,
-	onStartRequest: function(request, context) {
-		dump("Start");
-	},
-	onStopRequest: function(request, context, status) {
-		dump("Stop");
-		this.outputStream.close();
-		this.scriptableInputStream.close();
-	},
-	onDataAvailable: function(request, context, inputStream, offset, count) {
-		dump("Data");
-		let data = this.scriptableInputStream.read(count);
-		dump(data);
-		this._conv.writeMessage("IRC",data,{incoming: true});
-		dump("Data end");
-	},
-	
-	connect: function() {
-		this.base.connecting();
-		let self = this;
-		this._conv = new Conversation(self);
-		this._conv.writeMessage("IRC", "You're now chatting on IRC!", {system: true});
+  _conv: null,
+  inputStream: null,
+  outputStream: null,
+  scritableInputStream: null,
+  pump: null,
+  onStartRequest: function(request, context) {
+    dump("Start");
+  },
+  onStopRequest: function(request, context, status) {
+    dump("Stop");
+  },
+  onDataAvailable: function(request, context, inputStream, offset, count) {
+    dump("Data");
+    let data = this.scriptableInputStream.read(count);
+    dump(data);
+    this._conv.writeMessage("IRC",data,{incoming: true});
+    dump("Data end");
+  },
+  
+  connect: function() {
+    this.base.connecting();
+    let self = this;
+    this._conv = new Conversation(self);
+    this._conv.writeMessage("IRC", "You're now chatting on IRC!", {system: true});
 
-		var socketTransportService = Cc["@mozilla.org/network/socket-transport-service;1"].getService(Ci.nsISocketTransportService);
-		this.socketTransport = socketTransportService.createTransport(null, // Socket type
-																	 0, // Length of socket types
-																	 //"irc.mozilla.org", // Host
-																	 "localhost", // Host
-																	 6667, // Port
-																	 null); // Proxy info
-		
-		this.outputStream = this.socketTransport.openOutputStream(0, // flags
-																  0, // Use default segment size
-																  0); // Use default segment count
-		this.inputStream = this.socketTransport.openInputStream(0,
-																0, // Use default segment size
-																0); // Use default segment count
+    var socketTransportService = Cc["@mozilla.org/network/socket-transport-service;1"].getService(Ci.nsISocketTransportService);
+    this.socketTransport = socketTransportService.createTransport(null, // Socket type
+                                                                  0, // Length of socket types
+                                                                  "irc.mozilla.org", // Host
+                                                                  6667, // Port
+                                                                  null); // Proxy info
+    
+    this.outputStream = this.socketTransport.openOutputStream(0, // flags
+                                                              0, // Use default segment size
+                                                              0); // Use default segment count
+    this.inputStream = this.socketTransport.openInputStream(0, // flags
+                                                            0, // Use default segment size
+                                                            0); // Use default segment count
 
-		this.scriptableInputStream = Cc["@mozilla.org/scriptableinputstream;1"]
-										.createInstance(Ci.nsIScriptableInputStream);
-		this.scriptableInputStream.init(this.inputStream);
+    this.scriptableInputStream = Cc["@mozilla.org/scriptableinputstream;1"]
+                                   .createInstance(Ci.nsIScriptableInputStream);
+    this.scriptableInputStream.init(this.inputStream);
 
-		this.pump = Cc["@mozilla.org/network/input-stream-pump;1"]
-					  .createInstance(Ci.nsIInputStreamPump);
-		this.pump.init(this.inputStream, // Data to read
-					   -1, // Current offset
-					   -1, // Read all data
-					   0, // Use default segment size
-					   0, // Use default segment length
-					   false); // Do not close when done
-		this.pump.asyncRead(this, null);
+    this.pump = Cc["@mozilla.org/network/input-stream-pump;1"]
+                  .createInstance(Ci.nsIInputStreamPump);
+    this.pump.init(this.inputStream, // Data to read
+                   -1, // Current offset
+                   -1, // Read all data
+                   0, // Use default segment size
+                   0, // Use default segment length
+                   false); // Do not close when done
+    this.pump.asyncRead(this, null);
 
-		this.base.connected();
-	},
-	
-	disconnect: function() {
-		this.outputStream.close();
-		this.inputStream.close();
-		this.socketTransport.close(Components.results.NS_OK);
-	}
+    this.base.connected();
+  },
+  
+  disconnect: function() {
+    this.outputStream.close();
+    this.inputStream.close();
+    this.socketTransport.close(Components.results.NS_OK);
+  }
 };
 Account.prototype.__proto__ = GenericAccountPrototype;
 
 function IRCProtocol() { }
 IRCProtocol.prototype = {
-	get name() "IRC-JS",
-	getAccount: function(aKey, aName) new Account(this, aKey, aName),
-	classID: Components.ID("{607b2c0b-9504-483f-ad62-41de09238aec}"),
+  get name() "IRC-JS",
+  getAccount: function(aKey, aName) new Account(this, aKey, aName),
+  classID: Components.ID("{607b2c0b-9504-483f-ad62-41de09238aec}"),
 };
 IRCProtocol.prototype.__proto__ = GenericProtocolPrototype;
 
